fe700be6287a579c1e06ba8d2fa4dcd6b278f6a3
Kyle Rudderforth <kruddzz@gmail.com>
2020-07-14 21:46:40 -0500

    fixed error in htmlRoutes.js

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-
 // DEPENDENCIES
-const fs = require("fs");
-const path = require("path");
-const fs = require("fs");
+
 // EXPRESS CONFIGURATION
-const express = require("express");
+var express = require("express");
+
 // Tells node that we are creating an "express" server
-const app = express();
+var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(express.static("db"));
+
 
 // ROUTER
 
